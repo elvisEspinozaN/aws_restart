9cf6b265dad6e2926baa755c18c79aca87ad24a3
Elvis Espinoza Navarrete <elvis_espinoza98@yahoo.com>
2022-10-03 12:02:10 -0400

    Add files via upload

---
+++ lists-tuples-dict.py
@@ -0,0 +1,36 @@
+"""
+Your module description
+"""
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+myFinalAnswerTuple= ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+myFavoriteFruitDictonary = {
+    "elvis" : "apple",
+    "sophia" : "banana",
+    "steven" : "pineapple"
+}
+
+print(myFavoriteFruitDictonary)
+print(type(myFavoriteFruitDictonary))
+
+print(myFavoriteFruitDictonary["elvis"])
+print(myFavoriteFruitDictonary["sophia"])
+print(myFavoriteFruitDictonary["steven"])
+
